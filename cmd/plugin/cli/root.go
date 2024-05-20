74f419b01bbb2b990e8de2c8c0cda725b439f2ce
Bartosz Fenski <fenio@debian.org>
2024-05-20 18:57:28 +0200

    formatting

--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -1,46 +1,44 @@
 package cli
 
 import (
-    "os"
-    "path/filepath"
-    "strings"
+	"os"
+	"path/filepath"
+	"strings"
 
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
-    "k8s.io/cli-runtime/pkg/genericclioptions"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 var (
-    KubernetesConfigFlags *genericclioptions.ConfigFlags
+	KubernetesConfigFlags *genericclioptions.ConfigFlags
 )
 
 func RootCmd() *cobra.Command {
-    cmd := &cobra.Command{
-        Use:   "pv-mounter",
-        Short: "A tool to mount and unmount PVs",
-        Long:  `A tool to mount and unmount PVs using SSHFS.`,
-    }
+	cmd := &cobra.Command{
+		Use:   "pv-mounter",
+		Short: "A tool to mount and unmount PVs",
+		Long:  `A tool to mount and unmount PVs using SSHFS.`,
+	}
 
-    if strings.HasPrefix(filepath.Base(os.Args[0]), "kubectl-") {
-        cmd.Use = "kubectl pv-mounter [flags]"
-    }
+	if strings.HasPrefix(filepath.Base(os.Args[0]), "kubectl-") {
+		cmd.Use = "kubectl pv-mounter [flags]"
+	}
 
-    cmd.AddCommand(mountCmd())
-    cmd.AddCommand(cleanCmd())
+	cmd.AddCommand(mountCmd())
+	cmd.AddCommand(cleanCmd())
 
-    return cmd
+	return cmd
 }
 
 func InitAndExecute() {
-    KubernetesConfigFlags = genericclioptions.NewConfigFlags(true)
-    KubernetesConfigFlags.AddFlags(RootCmd().PersistentFlags())
+	KubernetesConfigFlags = genericclioptions.NewConfigFlags(true)
+	KubernetesConfigFlags.AddFlags(RootCmd().PersistentFlags())
 
-    viper.AutomaticEnv()
-    viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
-    if err := RootCmd().Execute(); err != nil {
-        os.Exit(1)
-    }
+	if err := RootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
 }
-
-

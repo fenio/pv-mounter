79ecf96ec2188cce7ed7a6f81ee5f46aadfcffdf
Bartosz Fenski <fenio@debian.org>
2024-05-20 17:44:32 +0200

    follow best practice for help output

--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -1,39 +1,46 @@
 package cli
 
 import (
-	"os"
-	"strings"
+    "os"
+    "path/filepath"
+    "strings"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
+    "k8s.io/cli-runtime/pkg/genericclioptions"
 )
 
 var (
-	KubernetesConfigFlags *genericclioptions.ConfigFlags
+    KubernetesConfigFlags *genericclioptions.ConfigFlags
 )
 
 func RootCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "pv-mounter",
-		Short: "A plugin to mount and clean PVCs",
-		Long:  `A plugin to mount and clean PVCs using SSHFS.`,
-	}
+    cmd := &cobra.Command{
+        Use:   "pv-mounter",
+        Short: "A tool to mount and unmount PVs",
+        Long:  `A tool to mount and unmount PVs using SSHFS.`,
+    }
 
-	cmd.AddCommand(mountCmd())
-	cmd.AddCommand(cleanCmd())
+    if strings.HasPrefix(filepath.Base(os.Args[0]), "kubectl-") {
+        cmd.Use = "kubectl pv-mounter [flags]"
+    }
 
-	return cmd
+    cmd.AddCommand(mountCmd())
+    cmd.AddCommand(cleanCmd())
+
+    return cmd
 }
 
 func InitAndExecute() {
-	KubernetesConfigFlags = genericclioptions.NewConfigFlags(true)
-	KubernetesConfigFlags.AddFlags(RootCmd().PersistentFlags())
+    KubernetesConfigFlags = genericclioptions.NewConfigFlags(true)
+    KubernetesConfigFlags.AddFlags(RootCmd().PersistentFlags())
 
-	viper.AutomaticEnv()
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+    viper.AutomaticEnv()
+    viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 
-	if err := RootCmd().Execute(); err != nil {
-		os.Exit(1)
-	}
+    if err := RootCmd().Execute(); err != nil {
+        os.Exit(1)
+    }
 }
+
+

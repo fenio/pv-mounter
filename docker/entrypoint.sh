d1c3f85104c88cea08e7fcff397cc374bd3513dc
Bartosz Fenski <fenio@debian.org>
2024-05-26 12:31:45 +0200

    SSH_PORT handling in docker image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,11 @@
 mkdir -p /home/ve/.ssh
 chmod 700 /home/ve/.ssh
 
+if [ -z "${SSH_PORT}" ]; then
+  # Define the variable
+  SSH_PORT="2137"
+fi
+
 # Check if SSH_KEY environment variable is set
 if [ -n "$SSH_KEY" ]; then
     echo "$SSH_KEY" >> /home/ve/.ssh/authorized_keys
@@ -18,20 +23,20 @@ chown -R ve:ve /home/ve/.ssh
 case "$ROLE" in
     standalone)
         echo "Running as standalone"
-        /usr/sbin/sshd -D -e
+        /usr/sbin/sshd -D -e -p $SSH_PORT
         tail -f /dev/null
         ;;
     proxy)
         echo "Running as proxy"
-        /usr/sbin/sshd -D -e
+        /usr/sbin/sshd -D -e -p $SSH_PORT
         ;;
     ephemeral)
         echo "Running as ephemeral"
-        /usr/sbin/sshd -D-e
+        /usr/sbin/sshd -D -e -p $SSH_PORT
         ;;
     *)
         echo "Running default..."
-        /usr/sbin/sshd -D -e
+        /usr/sbin/sshd -D -e -p $SSH_PORT
         ;;
 esac
 

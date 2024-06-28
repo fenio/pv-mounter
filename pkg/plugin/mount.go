661d57605746a1c9bb77574d25de6ef1f25f2c48
Bartosz Fenski <fenio@debian.org>
2024-06-28 18:23:21 +0200

    better error handling for key generation

--- pkg/plugin/mount.go
+++ pkg/plugin/mount.go
@@ -60,8 +60,7 @@ func Mount(namespace, pvcName, localMountPoint string, needsRoot bool) error {
 	// Generate the key pair once and use it for both standalone and proxy scenarios
 	privateKey, publicKey, err := GenerateKeyPair(elliptic.P256())
 	if err != nil {
-		fmt.Printf("Error generating key pair: %v\n", err)
-		return err
+        	return fmt.Errorf("error generating key pair: %v", err)
 	}
 
 	//	fmt.Printf("Private Key: %s\n", privateKey)

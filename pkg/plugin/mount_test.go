4cc75c27900c3ea623ef95ee858cbd5a6c119a74
Bartosz Fenski <fenio@debian.org>
2024-06-28 07:18:40 +0200

    different approach

--- pkg/plugin/mount_test.go
+++ pkg/plugin/mount_test.go
@@ -2,7 +2,9 @@ package plugin
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
@@ -16,9 +18,36 @@ func runCommand(cmdStr string) error {
 }
 
 func TestMountPVC(t *testing.T) {
-	// Run the mount command
-	err := Mount("default", "test-pvc", "/home/runner/work/pv-mounter/pv-mounter/foo", false)
-	if err != nil {
+	namespace := "default"
+	pvcName := "test-pvc"
+	localMountPoint := "/home/runner/work/pv-mounter/pv-mounter/foo"
+
+	// Run the mount command using the tool
+	mountCmd := fmt.Sprintf("./pv-mounter mount %s %s %s", namespace, pvcName, localMountPoint)
+	if err := runCommand(mountCmd); err != nil {
 		t.Fatalf("Failed to mount PVC: %v", err)
+	}
+
+	// Verify that the filesystem is writable
+	testFilePath := filepath.Join(localMountPoint, "testfile.txt")
+	testData := []byte("This is a test file.")
+	err := ioutil.WriteFile(testFilePath, testData, 0644)
+	if err != nil {
+		t.Fatalf("Failed to write to the mounted filesystem: %v", err)
+	}
+
+	// Verify that the file was written correctly
+	readData, err := ioutil.ReadFile(testFilePath)
+	if err != nil {
+		t.Fatalf("Failed to read the test file from the mounted filesystem: %v", err)
+	}
+	if string(readData) != string(testData) {
+		t.Fatalf("Data read from the test file does not match expected data: got %s, want %s", string(readData), string(testData))
+	}
+
+	// Run the unmount command using the tool
+	unmountCmd := fmt.Sprintf("./pv-mounter clean %s", localMountPoint)
+	if err := runCommand(unmountCmd); err != nil {
+		t.Fatalf("Failed to unmount the PVC: %v", err)
 	}
 }

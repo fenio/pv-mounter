09b8eefcc88e0d115ebdc6652ade0e0e9480ae1a
Bartosz Fenski <fenio@debian.org>
2026-02-16 22:28:15 +0100

    fix: bind-mount /volume so VFS FSAL can resolve it
    Ganesha's VFS FSAL requires the export path to appear as a distinct
    mount point in /proc/mounts. Bind-mounting /volume onto itself before
    starting ganesha.nfsd makes it visible in the mount table.

--- docker-nfs/entrypoint.sh
+++ docker-nfs/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Bind-mount /volume onto itself so it appears as a distinct mount point
+# in /proc/mounts. Ganesha's VFS FSAL requires the export path to be
+# resolvable via the mount table.
+mount --bind /volume /volume
+
 exec /usr/bin/ganesha.nfsd -F -L /dev/stderr -f /etc/ganesha/ganesha.conf
